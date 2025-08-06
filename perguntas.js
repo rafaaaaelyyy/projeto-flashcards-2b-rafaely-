6bcb22c98b497a526b09e0685c8fc6ae6730ed93
rafaaaaelyyy <l.rafaely@escola.pr.gov.br>
2025-08-06 12:17:33 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -6,7 +6,7 @@ criaCartao('Jogos Vorazes', 'Quem é o verdadeiro símbolo da rebelião nos Jogo
 
 criaCartao('Um de Nós Está Mentindo', 'Qual a teoria por trás da morte de Simon?', 'Várias teorias surgem, mas a verdade envolve manipulação, segredos e um plano cuidadosamente orquestrado.');
 
-criaCartao('O Jantar Secreto', 'O que une os convidados no jantar misterioso?', 'Todos os convidados têm uma ligação com um segredo sobre a vida de Leonardo da Vinci.');
+criaCartao('O Jantar Secreto', 'Quem são os amigos que se juntam para organizar os jantares?', 'Dantes, Miguel, Leitão e Hugo.');
 
 criaCartao('Dom Casmurro', 'Qual é o possível mistério em Dom Casmurro?', 'Se Capitu traiu ou não Bentinho com Escobar.');
 
